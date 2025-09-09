ffe714c46e2eed3b402b2974bee42e9ce6a2fe2f
dr34m14 <shopno155214@gmail.com>
2025-09-10 03:17:03 +0600

    2132.b solved

--- codeforces/2132.B.cpp
+++ codeforces/2132.B.cpp
@@ -3,42 +3,37 @@
 
 //B. The Secret Number
 
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    vector<long long int> arr;
-    long long int t,n;
-    cin>>t;
-    while (t--){
-        cin>>n;
-        long long int total_have=0;
-        if (n%11!=0){
-            cout<<0<<endl;
-        }else{
-            long long int total_loop=to_string(abs(n)).length()-1;
-            for(long long int i =0;i <=total_loop;i++)
-            {
+int main() {
+    int t;
+    cin >> t;
 
-                if(n%(1+(long long int)pow(10,i))==0)
-                {
-                    total_have++;
-                    arr.push_back(n/(1+(long long int)pow(10,i)));
+    while (t--) {
+        long long n;
+        cin >> n;
 
+        vector<long long> arr;
+        int num_digits = to_string(n).length();
 
-                }
+        long long base = 10;
+        for (int i = 1; i < num_digits; ++i) {
+            long long divisor = 1 + base;
+
+            if (n % divisor == 0) {
+                arr.push_back(n / divisor);
             }
-            cout<<total_have<<endl;
-            for (int i  = 0;  i< arr.size(); i++)
-            {
-        
-                cout<<arr[i]<<" ";    
-            }
-            arr.clear();
-            cout<<endl;
 
+            base *= 10;
         }
+
+        cout << arr.size() << endl;
+        sort(arr.begin(), arr.end());
+        for (long long x : arr) {
+            cout << x << " ";
+        }
+        cout << endl;
     }
 
     return 0;
